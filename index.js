23dac4be53e6724aeaf9f5af4701b1f55ef0d9ba
Seal Security <devops-admin@sealsecurity.io>
2024-09-04 12:45:46 +0000

    patched v1.0.0

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
-'use strict';
+function trimNewlines(string) {
+    var start = 0;
+    var end = string.length;
 
-var fn = module.exports = function (x) {
-	return fn.end(fn.start(x));
+    while (start < end && (string[start] === '\r' || string[start] === '\n')) {
+        start++;
+    }
+
+    while (end > 0 && (string[end - 1] === '\r' || string[end - 1] === '\n')) {
+        end--;
+    }
+
+    return (start > 0 || end < string.length) ? string.slice(start, end) : string;
+}
+
+trimNewlines.start = function (string) {
+    var end = string.length;
+    var start = 0;
+
+    while (start < end && (string[start] === '\r' || string[start] === '\n')) {
+        start++;
+    }
+
+    return start > 0 ? string.slice(start, end) : string;
 };
 
-fn.start = function (x) {
-	return x.replace(/^[\r\n]+/, '');
+trimNewlines.end = function (string) {
+    var end = string.length;
+
+    while (end > 0 && (string[end - 1] === '\r' || string[end - 1] === '\n')) {
+        end--;
+    }
+
+    return end < string.length ? string.slice(0, end) : string;
 };
 
-fn.end = function (x) {
-	return x.replace(/[\r\n]+$/, '');
-};
+module.exports = trimNewlines;
--- test.js
+++ test.js
@@ -1,28 +1,68 @@
-import test from 'ava';
-import fn from './';
+var test = require('ava');
+var trimNewlines = require('./index.js');
 
-test('main', t => {
-	t.is(fn('\nx\n'), 'x');
-	t.is(fn('\n\n\nx\n\n\n'), 'x');
-	t.is(fn('\r\nx\r\n'), 'x');
-	t.is(fn('\n\r\n\nx\n\r\n\n'), 'x');
-	t.end();
+test('main', function(t) {
+    t.is(trimNewlines(''), '');
+    t.is(trimNewlines('  '), '  ');
+    t.is(trimNewlines('\n\n\r'), '');
+    t.is(trimNewlines('\nx\n'), 'x');
+    t.is(trimNewlines('\n\n\nx\n\n\n'), 'x');
+    t.is(trimNewlines('\r\nx\r\n'), 'x');
+    t.is(trimNewlines('\n\r\n\nx\n\r\n\n'), 'x');
 });
 
-test('start', t => {
-	t.is(fn.start('\nx'), 'x');
-	t.is(fn.start('\r\nx'), 'x');
-	t.is(fn.start('\n\n\n\nx'), 'x');
-	t.is(fn.start('\n\n\r\n\nx'), 'x');
-	t.is(fn.start('x\n\n\r\n\n'), 'x\n\n\r\n\n');
-	t.end();
+test('start', function(t) {
+    t.is(trimNewlines.start(''), '');
+    t.is(trimNewlines.start('  '), '  ');
+    t.is(trimNewlines.start('\n\n\r'), '');
+    t.is(trimNewlines.start('\nx'), 'x');
+    t.is(trimNewlines.start('\r\nx'), 'x');
+    t.is(trimNewlines.start('\n\n\n\nx'), 'x');
+    t.is(trimNewlines.start('\n\n\r\n\nx'), 'x');
+    t.is(trimNewlines.start('x\n\n\r\n\n'), 'x\n\n\r\n\n');
 });
 
-test('end', t => {
-	t.is(fn.end('x\n'), 'x');
-	t.is(fn.end('x\r\n'), 'x');
-	t.is(fn.end('x\n\n\n\n'), 'x');
-	t.is(fn.end('x\n\n\r\n\n'), 'x');
-	t.is(fn.end('\n\n\r\n\nx'), '\n\n\r\n\nx');
-	t.end();
+test('end', function(t) {
+    t.is(trimNewlines.end(''), '');
+    t.is(trimNewlines.end('  '), '  ');
+    t.is(trimNewlines.end('\n\n\r'), '');
+    t.is(trimNewlines.end('x\n'), 'x');
+    t.is(trimNewlines.end('x\r\n'), 'x');
+    t.is(trimNewlines.end('x\n\n\n\n'), 'x');
+    t.is(trimNewlines.end('x\n\n\r\n\n'), 'x');
+    t.is(trimNewlines.end('\n\n\r\n\nx'), '\n\n\r\n\nx');
+});
+
+// Replace Array.from() and let/const with older approaches
+test('main - does not have exponential performance', function(t) {
+    for (var index = 0; index < 45000; index += 1000) {
+        var newlines = Array(index + 1).join('\n');
+        var string = newlines + 'a' + newlines;
+        var start = Date.now();
+        trimNewlines(string);
+        var difference = Date.now() - start;
+        t.true(difference < 10, 'Execution time: ' + difference);
+    }
+});
+
+test('start - does not have exponential performance', function(t) {
+    for (var index = 0; index < 45000; index += 1000) {
+        var newlines = Array(index + 1).join('\n');
+        var string = newlines + 'a';
+        var start = Date.now();
+        trimNewlines.start(string);
+        var difference = Date.now() - start;
+        t.true(difference < 10, 'Execution time: ' + difference);
+    }
+});
+
+test('end - does not have exponential performance', function(t) {
+    for (var index = 0; index < 45000; index += 1000) {
+        var newlines = Array(index + 1).join('\n');
+        var string = 'a' + newlines;
+        var start = Date.now();
+        trimNewlines.end(string);
+        var difference = Date.now() - start;
+        t.true(difference < 10, 'Execution time: ' + difference);
+    }
 });
